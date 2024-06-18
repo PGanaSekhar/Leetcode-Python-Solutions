108c5ef3115afe241df05f8f019e523801f5343c
PGanaSekhar <101617845+PGanaSekhar@users.noreply.github.com>
2024-06-18 21:53:22 +0530

    Create 826. Most Profit Assigning Work.py

---
+++ 826. Most Profit Assigning Work.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        res, j, best, temp = 0, 0, 0, []
+        for i in range(len(worker)):
+            temp.append((difficulty[i], profit[i]))
+        temp.sort()
+        worker.sort()
+        for i in worker:
+            while j < len(temp) and i >= temp[j][0]:
+                best = max(best, temp[j][1])
+                j += 1
+            res += best
+        return res
